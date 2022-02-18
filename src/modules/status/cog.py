1ecb887e8c7036ef6eb3e8eaf0aae556a1172e29
Stévan Corre <stevancorre@protonmail.com>
2022-02-18 19:28:24 +0100

    Using new client class

--- src/modules/status/cog.py
+++ src/modules/status/cog.py
@@ -1,8 +1,9 @@
 from nextcord.ext.commands import Cog, command
-from nextcord.ext.commands.bot import Bot, Context
+from client import Client
 
 from scrapper import ScrapperResult, get_servers_statuses
 from helper import get_ttl_hash, try_getenv
+from nextcord.ext.commands.bot import Bot, Context
 
 from modules.status.view import RegionsDropdownView
 from modules.status.embeds import ServerStatusEmbed
@@ -11,8 +12,8 @@ from modules.status.embeds import ServerStatusEmbed
 class Status(Cog):
     """Contains status commands"""
 
-    def __init__(self, bot: Bot):
-        self.bot = bot
+    def __init__(self, client: Client):
+        self.client = client
 
     @command(name="status")
     async def executeAsync(self, context: Context) -> None:
@@ -22,5 +23,5 @@ class Status(Cog):
         view.message = await context.reply(embed=ServerStatusEmbed(data.regions[0], data.last_updated), view=view)
 
 
-def setup(bot: Bot):
-    bot.add_cog(Status(bot))
+def setup(client: Client):
+    client.add_cog(Status(client))
